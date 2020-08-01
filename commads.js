cc574ab11f70a60b2cb0b786c569d73afe863b1a
sandeepkhore <sandeep@moreyeahs.in>
2020-08-01 12:43:48 +0530

    added frequently used commands

---
+++ commads.js
@@ -0,0 +1,41 @@
+/**
+ * this is to initialize repository
+ * git init to initiate the repository (if you do not have a repository)
+ * if you have a repository use (git clone https://github.com/SandeepKhore/github-session)
+ * 
+ * to set user name
+ * git config user.name ""
+ * git config user.email ""
+ * 
+ * 
+ * to create new branch
+ * git branch [branch name]
+ * git checkout -b [branch name]
+ * 
+ * to switch a branch
+ * git checkout [branch name]
+ * 
+ * to merge a branch
+ * either user
+ * git merge [branch name]
+ * or create a pull request and merge in master
+ * 
+ * to get the latest changes from remote repository to local
+ * git pull
+ * 
+ * 
+ * to commit and push the changes
+ * git add [file name] or git add .
+ * git commit -m "message here"
+ * git push origin [branch name] 
+ * 
+ * 
+ * to check the status (which all files have the uncommited changes)
+ * git status
+ * 
+ * to check the difference (current branch)
+ * git diff (to check all files changes)
+ * 
+ * to check difference between branches
+ * git diff branchA...branchB
+ */
